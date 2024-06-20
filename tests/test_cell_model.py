cca977bef9f661d0adb04b571b5ea5836aa39676
Austin Bryan <w.austin.bryan@gmail.com>
2024-06-19 22:14:53 -0400

    Added test cell model

---
+++ tests/test_cell_model.py
@@ -0,0 +1,127 @@
+﻿import unittest
+from models.cell_model import CellModel
+from models.cell_value_type import CellValueType
+
+
+class TestCellModel(unittest.TestCase):
+    def setUp(self):
+        self.cell_model = CellModel(0, 0)
+
+    def test_init(self):
+        """ Asserts the initial state of the cell model."""
+        self.assertEqual(self.cell_model.x, 0)
+        self.assertEqual(self.cell_model.y, 0)
+        self.assertIsNone(self.cell_model.value)
+        self.assertEqual(self.cell_model.notes, [False for _ in range(9)])
+        self.assertFalse(self.cell_model.in_conflict)
+        self.assertEqual(self.cell_model.value_type, CellValueType.BLANK)
+
+    def test_toggle_entry(self):
+        """ Asserts that toggling an entry on and off works."""
+        self.cell_model.toggle_entry(5)
+        self.assertEqual(self.cell_model.value, 5)
+        self.assertEqual(self.cell_model.value_type, CellValueType.ENTRY)
+
+        self.cell_model.toggle_entry(5)
+        self.assertIsNone(self.cell_model.value)
+        self.assertEqual(self.cell_model.value_type, CellValueType.BLANK)
+
+    def test_toggle_entry_given(self):
+        """ Tests that toggling entries have no effect on a given."""
+        self.cell_model.set_given(7)
+        self.cell_model.toggle_entry(5)
+        self.assertEqual(self.cell_model.value, 7)
+        self.assertEqual(self.cell_model.value_type, CellValueType.GIVEN)
+
+    def test_set_given(self):
+        """ Tests that setting the given sets the value and value type properly."""
+        self.cell_model.set_given(8)
+        self.assertEqual(self.cell_model.value, 8)
+        self.assertEqual(self.cell_model.value_type, CellValueType.GIVEN)
+
+    def test_toggle_note(self):
+        """ Assures that toggling notes on and off works."""
+        self.cell_model.toggle_note(3)
+        self.assertTrue(self.cell_model.notes[2])
+        self.assertEqual(self.cell_model.value_type, CellValueType.NOTES)
+
+        self.cell_model.toggle_note(3)
+        self.assertFalse(self.cell_model.notes[2])
+        self.assertEqual(self.cell_model.value_type, CellValueType.BLANK)
+
+    def test_clear(self):
+        """ Asserts that clear removes all notes and entry"""
+        self.cell_model.toggle_entry(3)
+        self.cell_model.toggle_note(2)
+        self.cell_model.toggle_note(4)
+        self.cell_model.toggle_note(6)
+        self.cell_model.clear()
+
+        self.assertIsNone(self.cell_model.value)
+        self.assertFalse(self.cell_model.in_conflict)
+        self.assertEqual(self.cell_model.notes, [False for _ in range(9)])
+        self.assertEqual(self.cell_model.value_type, CellValueType.BLANK)
+
+    def test_clear_given(self):
+        """ Ensures that clear cannot clear a given value"""
+        self.cell_model.set_given(9)
+        self.cell_model.clear()
+        self.assertEqual(self.cell_model.value, 9)
+        self.assertEqual(self.cell_model.value_type, CellValueType.GIVEN)
+
+    def test_toggle_note_given(self):
+        """ Ensures that toggling notes cannot clear given value"""
+        self.cell_model.set_given(4)
+        self.cell_model.toggle_note(3)
+        self.assertEqual(self.cell_model.value, 4)
+        self.assertEqual(self.cell_model.value_type, CellValueType.GIVEN)
+        self.assertFalse(self.cell_model.notes[2])
+
+    def test_is_entry(self):
+        self.cell_model.toggle_entry(1)
+        self.assertTrue(self.cell_model.is_entry())
+
+    def test_is_given(self):
+        self.cell_model.set_given(1)
+        self.assertTrue(self.cell_model.is_given())
+
+    def test_is_notes(self):
+        self.cell_model.toggle_note(2)
+        self.assertTrue(self.cell_model.is_notes())
+
+    def test_is_blank(self):
+        """ Tests that clearing resets to blank."""
+        self.assertTrue(self.cell_model.is_blank())
+        self.cell_model.toggle_entry(3)
+        self.cell_model.clear()
+        self.assertTrue(self.cell_model.is_blank())
+
+        self.cell_model.toggle_note(3)
+        self.cell_model.clear()
+        self.assertTrue(self.cell_model.is_blank())
+
+    def test_has_value(self):
+        """ Tests that setting givens or entries is reflected in has_value() """
+        self.assertFalse(self.cell_model.has_value())
+        self.cell_model.toggle_entry(4)
+        self.assertTrue(self.cell_model.has_value())
+
+        self.cell_model.clear()
+        self.assertFalse(self.cell_model.has_value())
+
+        self.cell_model.set_given(5)
+        self.assertTrue(self.cell_model.has_value())
+
+    def test_has_note(self):
+        """ Tests that toggling notes is reflected in has_note()"""
+        self.assertFalse(self.cell_model.has_note(1))
+        self.cell_model.toggle_note(1)
+        self.assertTrue(self.cell_model.has_note(1))
+
+    def test_note_cache(self):
+        """ Tests that note cache is not cleared after toggling on an entry."""
+        self.assertEqual(self.cell_model.notes, [False for _ in range(9)])
+        self.cell_model.toggle_note(1)
+        self.cell_model.toggle_entry(5)
+        self.assertEqual(self.cell_model.notes, [True] + [False for _ in range(8)])
+
--- tests/test_cell_view.py
+++ tests/test_cell_view.py
@@ -16,17 +16,17 @@ class TestCellView(unittest.TestCase):
         self.root.destroy()
 
     def test_initial_color(self):
-        """Test that the initial background color is set correctly."""
+        """ Test that the initial background color is set correctly."""
         self.assertEqual(self.cell_view['bg'], CellView._DEFAULT_COLOR)
 
     def test_update_labels_entry(self):
-        """Test that the entry value is displayed correctly and color is updated."""
+        """ Test that the entry value is displayed correctly and color is updated."""
         self.set_value(4, CellValueType.ENTRY)
         self.assertEqual(self.value_text, '4')
         self.assertEqual(self.value_fill, 'white')
 
     def test_update_labels_notes(self):
-        """Test that notes are displayed correctly and updated appropriately."""
+        """ Test that notes are displayed correctly and updated appropriately."""
         random.seed(42)  # Fix the seed for repeatability
         for _ in range(10):  # Test multiple scenarios
             notes_on = random.sample(range(9), k=random.randint(1, 9))
@@ -38,7 +38,7 @@ class TestCellView(unittest.TestCase):
 
             for i in range(9):
                 expected_text = str(i + 1) if i in notes_on else ''
-                self.assertEqual(self.cell_view.itemcget(self.cell_view.note_labels[i], 'text'), expected_text)
+                self.assertEqual(self.get_note_label(i), expected_text)
 
             notes_off = random.sample(notes_on, k=random.randint(0, len(notes_on)))
             for i in notes_off:
@@ -48,22 +48,22 @@ class TestCellView(unittest.TestCase):
 
             for i in range(9):
                 expected_text = str(i + 1) if self.model.notes[i] else ''
-                self.assertEqual(self.cell_view.itemcget(self.cell_view.note_labels[i], 'text'), expected_text)
+                self.assertEqual(self.get_note_label(i), expected_text)
 
     def test_clear_entry(self):
-        """Test that clearing the entry label works correctly."""
+        """ Test that clearing the entry label works correctly."""
         self.set_value(6, CellValueType.ENTRY)
         self.cell_view.clear_entry()
         self.assertEqual(self.cell_view.itemcget(self.cell_view.value_label, 'text'), '')
 
     def test_update_values_given(self):
-        """Test that given values are displayed correctly with the correct styling."""
+        """ Test that given values are displayed correctly with the correct styling."""
         self.set_value(7, CellValueType.GIVEN)
         self.assertEqual(self.value_text, '7')
         self.assertEqual(self.value_fill, 'black')
 
     def test_given_stays_in_view(self):
-        """Test that clearing a given, or replacing with a note or entry has no effect in the view."""
+        """ Test that clearing a given, or replacing with a note or entry has no effect in the view."""
         self.set_value(7, CellValueType.GIVEN)
         self.model.toggle_entry(3)
         self.cell_view.update_labels()
@@ -77,6 +77,45 @@ class TestCellView(unittest.TestCase):
         self.cell_view.update_labels()
         self.assertEqual(self.value_text, '7')  # Value should remain 7
 
+    def test_note_overriding(self):
+        """ Tests that notes can override entries."""
+        self.set_value(7, CellValueType.ENTRY)
+        self.cell_view.update_labels()
+
+        # Test that notes can hide entries
+        self.model.toggle_entry(4)
+        self.model.toggle_note(1)
+        self.cell_view.update_labels()
+        self.assertEqual(self.value_text, '')
+
+        # Test that entries will hide notes
+        self.model.toggle_note(2)
+        self.model.toggle_note(3)
+        self.cell_view.update_labels()
+
+        self.model.toggle_entry(2)
+        self.cell_view.update_labels()
+
+        for i in range(9):
+            self.assertEqual(self.get_note_label(i), '')
+
+        # Test that toggling entries off will restore notes
+        self.model.toggle_entry(self.model.value)
+        self.cell_view.update_labels()
+        self.assertEqual(self.value_text, '')
+        for i in range(3):
+            self.assertEqual(self.get_note_label(i), str(i + 1))
+
+        # Test that notes can be deleting when switching from entry back to notes
+        self.model.toggle_entry(5)
+        self.cell_view.update_labels()
+        self.model.toggle_note(3)
+        self.cell_view.update_labels()
+
+        for i in range(2):
+            self.assertEqual(self.get_note_label(i), str(i + 1))
+        self.assertEqual(self.get_note_label(2), '')
+        
     #
     # Helper Methods
     #
@@ -93,6 +132,9 @@ class TestCellView(unittest.TestCase):
     @property
     def value_fill(self):
         return self.cell_view.itemcget(self.cell_view.value_label, 'fill')
+
+    def get_note_label(self, index):
+        return self.cell_view.itemcget(self.cell_view.note_labels[index], 'text')
 
 
 if __name__ == '__main__':
