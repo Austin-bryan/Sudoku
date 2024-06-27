f9e85b24a0a77499a61a0383d740bc48c0b96314
Austin Bryan <w.austin.bryan@gmail.com>
2024-06-26 22:12:06 -0400

    stream lined game creation

--- main.py
+++ main.py
@@ -35,49 +35,35 @@ class SudokuApp:
 
         # Attach the conflict observer
         self.conflict_observer = ConflictObserver(self.board_controller.model)
-        self.backtracking_solver = BacktrackingSolver(self.board_controller)
+        self.backtracking_solver = BacktrackingSolver(self.board_controller, ui_display_mode=True)
 
         # Create Difficulty UI
         self.difficulty_label = tk.Label(self.difficult_row, text="Difficulty:", bg=BACKGROUND_COLOR,
                                          fg="white", font=("Helvetica", 12), anchor="w")
+        self.generator = None
 
         def easy_command(event):
-            generator = SudokuGenerator(self.board_controller, 25)
-            generator.generate_board()
+            self.generator = SudokuGenerator(self.board_controller, 40)
+            self.generator.generate_board()
 
         def medium_command(event):
-            generator = SudokuGenerator(self.board_controller, 30)
-            generator.generate_board()
+            self.generator = SudokuGenerator(self.board_controller, 45)
+            self.generator.generate_board()
 
         def hard_command(event):
-            generator = SudokuGenerator(self.board_controller, 40)
-            generator.generate_board()
+            self.generator = SudokuGenerator(self.board_controller, 50)
+            self.generator.generate_board()
 
         def extreme_command(event):
-            generator = SudokuGenerator(self.board_controller, 55)
-            generator.generate_board()
+            self.generator = SudokuGenerator(self.board_controller, 55)
+            self.generator.generate_board()
+
+        easy_command(None)
 
         options = [("Easy", easy_command), ("Medium", medium_command), ("Hard", hard_command), ("Extreme", extreme_command)]
 
         dropdown = DropdownMenu(self.difficult_row, options, width=125, height=40)
         dropdown.grid(row=0, column=1, sticky="w", padx=10, pady=10)
-
-    def open_popup(self, event):
-        popup = tk.Toplevel(root)
-        popup.title("Popup Window")
-        print(f"200x100+{self.new_game_button.winfo_rootx()}+{self.new_game_button.winfo_rooty() + 20}")
-        popup.geometry(f"200x100+{self.new_game_button.winfo_rootx()}+{self.new_game_button.winfo_rooty() + 20}")
-        popup.configure(bg=BACKGROUND_COLOR)  # Set the background color
-        popup.overrideredirect(True)  # Hide the title bar
-
-        # label = tk.Label(popup, text="This is a popup window")
-        # label.pack(pady=20)
-
-        # button = tk.Button(popup, text="Close", command=popup.destroy)
-        # button.pack(pady=10)
-
-        new_game_button = ActionButton(popup, "New Game", font_size=12, width=120, height=40)
-        new_game_button.grid(row=0, column=0, padx=40, pady=10)
 
     def create_widgets(self):
         """Create and configure the widgets for the Sudoku application."""
@@ -89,15 +75,15 @@ class SudokuApp:
 
         # Create action buttons
         padx = 2
-        self.create_action_button('Undo', 0, lambda event: self.undo_history_manager.undo(), padx)
-        self.create_action_button('Redo', 1, lambda event: self.undo_history_manager.redo(), padx)
+        self.create_action_button('Undo', 0, lambda e: self.undo_history_manager.undo(), padx)
+        self.create_action_button('Redo', 1, lambda e: self.undo_history_manager.redo(), padx)
         self.create_mode_button('Notes', 2, padx)
-        self.create_action_button('Clear', 3, lambda event: self.board_controller.clear_selected(), padx)
-        self.create_action_button('Hint', 4, lambda event: self.board_controller.clear_selected(), padx)
+        self.create_action_button('Clear', 3, lambda e: self.board_controller.clear_selected(), padx)
+        self.create_action_button('Hint', 4, lambda e: self.board_controller.clear_selected(), padx)
 
         # Create large buttons
-        self.new_game_button = self.create_large_button('New Game', row=1, pady=0, command=self.open_popup)
-        self.create_large_button('Solve', row=5, pady=10, command=lambda event: self.backtracking_solver.solve())
+        self.create_large_button('New Game', row=1, pady=0, command=lambda e: self.generator.generate_board())
+        self.create_large_button('Solve', row=5, pady=10, command=lambda e: self.backtracking_solver.solve())
 
     @staticmethod
     def create_frame(parent, row, column,
